5d2911ba82460be881de7b696970be1434e6818d
Devesh-k04 <105843151+Devesh-k04@users.noreply.github.com>
2022-05-24 14:55:16 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,16 @@
+def add(no1,no2):  # no1 and no2 denote the numbers on whom the arithmatic function is to be performed
+   sum = no1 + no2
+   print("the sum of", no1, "and", no2, "is", sum)
+def subtract(no1,no2):
+   subtraction = no1 - no2
+   print(no2, "subtracted from", no1, "is", subtraction)
+def multiply(no1,no2):
+    product = no1*no2
+    print("the product of", no1, "and", no2, "is", product)
+def divide(no1,no2):
+    division = no1/no2
+    print(no1,"divided by",no2,"is",division)
+add(2,5)
+subtract(7,5)
+multiply(5,10)
+divide(9,3)
---
+++ conversion.py
@@ -0,0 +1,4 @@
+def conversion(n1): #n1 denotes the temperature in degree celsius
+    n1f = (n1 * 9/5) +32
+    print(n1,"C is equal to",n1f,"F")
+conversion(50)
